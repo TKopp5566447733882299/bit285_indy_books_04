bd8020e086011d198c2ae1e99eeb37bc77b0e90f
Anthony Kopp <stickman2001@gmail.com>
2022-03-08 15:26:46 -0800

    Succsesfull Brouser Responce

---
+++ Controllers/WriterController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IndyBooks.Models;
+using IndyBooks.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IndyBooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WriterController : ControllerBase
+    {
+        // GET: api/<WriterController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<WriterController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<WriterController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WriterController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WriterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
